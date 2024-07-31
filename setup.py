459a4293f46c27a5190f2d581eb754b8394022ed
Blazej Banaszewski <blazejb@graphcore.ai>
2024-07-31 10:05:11 +0000

    small textual updates to the notebook

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='minimol',
-    version='1.1',
+    version='1.2',
     packages=find_packages(),
     include_package_data=True,
     package_data={
