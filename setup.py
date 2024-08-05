8db312ea78fa9102ec4f638f7109ca68ff86dbb9
Blazej Banaszewski <blazejb@graphcore.ai>
2024-08-05 11:49:33 +0000

    removing redudant ckpt folder

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='minimol',
-    version='1.3.1',
+    version='1.3.4',
     packages=find_packages(),
     include_package_data=True,
     package_data={
